d69b8dbe52907e6dc99423cc627697f5a87ef2c1
PyLii <pyotrkamyak@gmail.com>
2021-12-28 19:57:44 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Pyotr";
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
